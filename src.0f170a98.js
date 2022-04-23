56ca8ff1b90522695c058018d92103990780bfc7
Tripoli80 <35936980+Tripoli80@users.noreply.github.com>
2022-04-23 20:34:22 +0000

    Deploying to gh-pages from @ Tripoli80/goit-markup-hw-08@d4ec9cf1339abbeff09d4e822f7014efdfbb3a38 🚀

--- src.0f170a98.js
+++ src.0f170a98.js
@@ -7,4 +7,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./menu"),require("../js/modal");
 },{"./sass/main.scss":"clu1","./menu":"Z6FK","../js/modal":"Jxnb"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-markup-hw-08/src.2d214e88.js.map
+//# sourceMappingURL=/goit-markup-hw-08/src.0f170a98.js.map
